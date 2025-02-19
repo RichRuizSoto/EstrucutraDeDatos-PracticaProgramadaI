f5aebcfc6ddc1f3c87dad8bff9c7471a8fa0dd9c
RichRuizSoto <richardruizs1998@gmail.com>
2025-02-19 13:12:06 -0600

    PracticaProgramadaI.java

--- PracticaProgramadaI.java
+++ PracticaProgramadaI.java
@@ -1,8 +1,4 @@
-
 package com.mycompany.practicaprogramadai;
-
-import javax.swing.*;
-
 
 public class PracticaProgramadaI {
 
@@ -11,8 +7,43 @@ public class PracticaProgramadaI {
         String cadena = "11001110";
         int i = cadena.length();
         
-        System.out.println(i);
-                
-
+        
+        int[] arregloDeNumeros = {1, 2, 3, 4, 5, 6, 8};
+        
+        contadorBitsEncendidos(cadena, i, 0);
+        encontrarNumerosPares(arregloDeNumeros, 0);
+        
+        
     }
+
+    //Ejercicio #1
+    public int contadorBitsEncendidos(String cadena, int i, int contador) {
+        if (cadena.charAt(i).equals("1")) {
+            return contador + 1;
+        } else {
+            return contador;
+        }
+
+        if (cadena.charAt(i).equals("1")) {
+            contador++;
+        }
+
+        return contadorBitsEncendidos(cadena, i - 1, contador);
+    }
+    
+    //Ejercicio #2
+    public int encontrarNumerosPares(int[] arregloDeNumeros, int contador) {
+        for (int i = 0; i < arregloDeNumeros.length; i++) {
+            if (arregloDeNumeros[i] % 2 == 0) {
+                contador ++;
+            }
+        }
+        return contador;
+    }
+    
+    //Ejercicio #3
+    
+    
+    // LA RESOUESTA ES 7
 }
+
