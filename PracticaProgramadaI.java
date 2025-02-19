150be126a73b24b07f3ee7fe30f88608bea01a57
RichRuizSoto <richardruizs1998@gmail.com>
2025-02-19 13:11:26 -0600

    Add files via upload

---
+++ PracticaProgramadaI.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.practicaprogramadai;
+
+import javax.swing.*;
+
+
+public class PracticaProgramadaI {
+
+    public static void main(String[] args) {
+
+        String cadena = "11001110";
+        int i = cadena.length();
+        
+        System.out.println(i);
+                
+
+    }
+}
